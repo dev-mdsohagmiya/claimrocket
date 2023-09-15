27e79ff9ec333ff1ad2404023a2015a4606932c1
msbhh-ms <learnwithmsbhh@gmail.com>
2023-09-15 16:44:01 +0600

    updated login and claim-3

--- components/PrivacyPolicy.jsx
+++ components/PrivacyPolicy.jsx
@@ -8,7 +8,7 @@ const PrivacyPolicy = () => {
     day: "numeric",
   });
   return (
-    <div className="bg-gray-100 p-8 pt-[130px]">
+    <div className="bg-gray-100 p-8 pt-[130px] px-[45]">
       <div className="max-w-3xl mx-auto">
         <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
         <p className="text-gray-700 mb-4">Last Updated: [{currentDate}]</p>
