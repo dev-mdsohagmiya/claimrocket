35559c7570b06b5e21fa3a7b78a3ea353d0be28b
msbhh-ms <learnwithmsbhh@gmail.com>
2023-09-15 16:48:20 +0600

    updated blogs

--- components/Blogs.jsx
+++ components/Blogs.jsx
@@ -5,7 +5,7 @@ const Blogs = ({ title, description, imageUrl, date }) => {
   return (
     <div className="pt-[80px] ">
       <div className="mx-5">
-        <div className="bg-white shadow-xl overflow-hidden">
+        <div className="bg-[#1c1c1c]  shadow-xl overflow-hidden">
           <Image
             height={300}
             width={350}
@@ -14,9 +14,9 @@ const Blogs = ({ title, description, imageUrl, date }) => {
             className="w-full h-[300px] object-cover object-center "
           />
           <div className="p-6">
-            <h2 className="text-2xl font-semibold mb-4">{title}</h2>
-            <p className="text-gray-600 text-sm mb-2">{date}</p>
-            <p className="text-gray-700">{description}</p>
+            <h2 className="text-2xl font-semibold mb-4 text-white">{title}</h2>
+            <p className="text-[#757575] text-sm mb-2">{date}</p>
+            <p className="text-gray-100">{description}</p>
             <div className="mt-4">
               <button className="bg-[#E02B20] text-white font-semibold py-2 px-4  hover:bg-black transition duration-300">
                 Read More
